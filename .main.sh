d032c9d39173fa28c3a3663ca789f80811d3ddaf
Kqpa <epicmanborax@gmail.com>
2022-03-01 21:44:22 +0300

    improve debug

--- .main.sh
+++ .main.sh
@@ -7,15 +7,26 @@ UNIX_TIME=$(date '+%s')
 
 for sh ($DIRECTORY/**/*.sh); do
   
-  source $sh
-
     if [[ $DEBUG == "on" ]]; then
 
       if [ -d "$DIRECTORY/.debug" ]; then; else; mkdir $DIRECTORY/.debug; fi
       
       touch $DIRECTORY/.debug/$UNIX_TIME.log
-      echo "[SHELL: '$(basename $SHELL)'] - [LOGFILE: '$UNIX_TIME'] - LOADED: '$(basename $sh)' from '$(basename $(dirname $sh))'" >> $DIRECTORY/.debug/$UNIX_TIME.log
 
-    fi
+        if source $sh; then
+        
+          echo "[SHELL: '$(basename ${SHELL:u})'] - [LOGFILE: '$UNIX_TIME'] - [LOADED: '$(basename $sh)' from '$(basename $(dirname $sh))']" >> $DIRECTORY/.debug/$UNIX_TIME.log
+        
+        else
+
+          echo "\n<------------>" >> $DIRECTORY/.debug/$UNIX_TIME.log
+          echo "[SHELL: '$(basename ${SHELL:u})'] - [LOGFILE: '$UNIX_TIME'] - [FAILED TO LOAD: '$(basename $sh)' from '$(basename $(dirname $sh))']" >> $DIRECTORY/.debug/$UNIX_TIME.log
+          printf "ERROR: " >> $DIRECTORY/.debug/$UNIX_TIME.log
+          source $sh &>> $DIRECTORY/.debug/$UNIX_TIME.log
+          echo "<------------>\n" >> $DIRECTORY/.debug/$UNIX_TIME.log
+
+        fi
+
+    elif [[ $DEBUG == "off" ]]; then; source $sh; fi 
 
 done
