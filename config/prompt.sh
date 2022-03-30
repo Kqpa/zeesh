fef694cb2e40a90e5bd87ed6d38c37cbff746036
Kqpa <76552592+Kqpa@users.noreply.github.com>
2022-03-30 14:08:55 +0300

    changes to prompt

--- config/prompt.sh
+++ config/prompt.sh
@@ -2,4 +2,4 @@ autoload -U colors && colors
 
 setopt prompt_subst
 
-PS1='%{$fg[green]%}%n%{$reset_color%}@%m %{$fg[green]%}%(4~|%-1~/…/%2~|%4~) %{$reset_color%}$(prompt-branch)%{$fg[red]%}%(?..[%?] )%{$reset_color%}> '
+PS1='%{$fg[green]%}%n%{$reset_color%}@%m %{$fg[green]%}%(4~|%-1~/…/%2~|%4~) %{$reset_color%}$(prompt-branch)%(?..[%{$fg[red]%}%?%{$reset_color%}] )> '
