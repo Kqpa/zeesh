984413906d12af63913741f9c8cbbb1314c6563b
Kqpa <76552592+Kqpa@users.noreply.github.com>
2022-03-31 13:25:12 +0300

    FInAL prompt update

--- config/prompt-branch.sh
+++
@@ -1,7 +0,0 @@
-function prompt-branch() { 
-    
-    prompt_branch=$(git symbolic-ref HEAD 2> /dev/null | awk 'BEGIN{FS="/"} {print $NF}') 
-    
-    if [[ $prompt_branch == "" ]]; then : ;else echo '[\033[32m'$prompt_branch'\e[0m] ' ;fi 
-
-}
--- config/prompt.sh
+++ config/prompt.sh
@@ -2,4 +2,12 @@ autoload -U colors && colors
 
 setopt prompt_subst
 
-PS1='%{$fg[green]%}%n%{$reset_color%}@%m %{$fg[green]%}%(4~|%-1~/…/%2~|%4~) %{$reset_color%}$(prompt-branch)%(?..[%{$fg[red]%}%?%{$reset_color%}] )> '
+PS1='%{$fg[green]%}%n%{$reset_color%}@%m %{$fg[green]%}%(4~|%-1~/…/%2~|%4~) %{$reset_color%}$(prompt-branch)%(?..%{$fg[red]%}[%{$reset_color%}%{$fg_bold[red]%}%?%{$reset_color%}%{$fg[red]%}]%{$reset_color%} )> '
+
+function prompt-branch() { 
+    
+    prompt_branch=$(git symbolic-ref HEAD 2> /dev/null | awk 'BEGIN{FS="/"} {print $NF}') 
+    
+    if [[ $prompt_branch == "" ]]; then : ;else echo "[${fg[green]}$prompt_branch${reset_color}] " ;fi 
+
+}
