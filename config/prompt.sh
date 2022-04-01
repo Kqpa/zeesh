c4cc86948cd0d173b5fc3a7b327e6a110ba310b3
Kqpa <76552592+Kqpa@users.noreply.github.com>
2022-04-01 10:47:29 +0300

    indicate git modification

--- config/prompt.sh
+++ config/prompt.sh
@@ -4,10 +4,14 @@ setopt prompt_subst
 
 PS1='%{$fg[green]%}%n%{$reset_color%}@%m %{$fg[green]%}%(4~|%-1~/…/%2~|%4~) %{$reset_color%}$(prompt-branch)%(?..%{$fg[red]%}[%{$reset_color%}%{$fg_bold[red]%}%?%{$reset_color%}%{$fg[red]%}]%{$reset_color%} )> '
 
-function prompt-branch() { 
+function prompt-branch() {
+
+    has_modification=""
+
+    prompt_branch=$(git symbolic-ref HEAD 2> /dev/null | awk 'BEGIN{FS="/"} {print $NF}')
+
+    if [[ `git ls-files -dmo --exclude-standard 2> /dev/null` ]]; then; has_modification="!"; fi
     
-    prompt_branch=$(git symbolic-ref HEAD 2> /dev/null | awk 'BEGIN{FS="/"} {print $NF}') 
-    
-    if [[ $prompt_branch == "" ]]; then : ;else echo "[${fg[green]}$prompt_branch${reset_color}] " ;fi 
+    if [[ $prompt_branch == "" ]]; then : ;else echo "[$has_modification${fg[green]}$prompt_branch${reset_color}] " ;fi 
 
 }
