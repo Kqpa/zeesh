7161f260e0525128a0e22ce4c384c1e74007eccf
Kqpa <76552592+Kqpa@users.noreply.github.com>
2022-04-19 14:56:54 +0300

    edit executable in `$PATH`

---
+++ scripts/edit.sh
@@ -0,0 +1,15 @@
+# Quickly edit executables on your $PATH
+
+function edit() {
+
+	if [ -z "$1" ]; then
+    	echo "usage: edit <executable>"
+	elif [ -w $(which $1) ] && [ -f $(which $1) ]; then
+		vim $(which $1)
+	elif [ ! -w $(which $1) ] && [ -f $(which $1) ]; then
+		sudo vim $(which $1)
+	else
+		echo "edit: $1: No such file or directory"
+	fi
+
+}
