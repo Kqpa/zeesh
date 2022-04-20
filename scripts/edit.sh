a8a66c7d0f7394d1cd13a8139dede6eec8baa534
Kqpa <76552592+Kqpa@users.noreply.github.com>
2022-04-20 14:22:45 +0300

    improve `edit`

--- scripts/edit.sh
+++ scripts/edit.sh
@@ -4,12 +4,22 @@ function edit() {
 
 	if [[ -z "$1" ]]; then
     	echo "usage: edit <executable>"
-	elif [[ -w $(which $1) ]] && [[ -f $(which $1) ]]; then
-		vim $(which $1)
-	elif [[ ! -w $(which $1) ]] && [[ -f $(which $1) ]]; then
-		sudo vim $(which $1)
-	else
-		echo "edit: $1: No such file or directory"
+		return 0
+	elif [[ ! -f $(which $1) ]]; then
+		echo "edit: $1 is not a valid executable on '\$PATH'" 
+		return 1
 	fi
 
+	if [[ $(file -b --mime-type $(which $1) | sed 's|/.*||') == *"application"* ]]; then
+			xxd $(which $1) | vim -
+	elif [[ $(file -b --mime-type $(which $1) | sed 's|/.*||') == *"text"* ]]; then
+		if [[ -w $(which $1) ]]; then
+			vim $(which $1)
+		else
+			sudo vim $(which $1)
+		fi
+	else
+		echo "edit: $1: Not a valid file format"
+		return 1
+	fi
 }
