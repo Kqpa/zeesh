ae80a7e1411e34c3a6ca0f49afc1913d877dcbaa
Kqpa <76552592+Kqpa@users.noreply.github.com>
2022-09-06 01:45:47 +0300

    Minor fix

--- .main.sh
+++ .main.sh
@@ -66,11 +66,11 @@ __ZEESH_LOAD_INFO="[${fg[$__ZEESH_ACCENT_COLOR]}zeesh${reset_color}::${fg[$__ZEE
 
 case $__ZSH_PROMPT in
 
+    "default") __load_default_prompt ;;
+    "random") __load_random_prompt ;;
     "classic") __load_classic_prompt ;;
     "minimal") __load_minimal_prompt ;;
-    "default") __load_default_prompt ;;
     "space") __load_space_prompt ;;
-    "random") __load_random_prompt ;;
     *) echo "$__ZEESH_LOAD_INFO $__ZSH_PROMPT: not a prompt, applying default prompt" && __load_default_prompt ;;
 
 esac
@@ -78,6 +78,7 @@ esac
 case $__TTY_THEME in
 
     "default") __load_default_tty ;;
+    "random") __load_random_tty ;;
     "dracula-dark") __load_dracula_dark_tty ;;
     "soft-dark") __load_soft_dark_tty ;;
     "solarized-dark") __load_solarized_dark_tty ;;
